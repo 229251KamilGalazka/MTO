0c3bbf582b4c786a3dae18005bb89d85025dc6ed
Kamil Galazka <you@example.com>
2022-10-09 14:18:03 +0200

    add lab2 folder

---
+++ lab2/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int my_printf(char *format_string, char *param){
+	for(int i=0;i<strlen(format_string);i++){
+		if((format_string[i] == '#') && (format_string[i+1] == 'k')){
+		for(int j=0; j<strlen(param);i++{
+			if(islower(param[j]){
+				printf("%c", toupper(param[j]))
+			}else{
+				printf("%c", tolower(param[j]))
+			}
+		}
+		}else
+			putchar(format_string[i]);
+	}
+	puts("");
+}
+
+int main(int argc, char *argv[]){
+	char buf[1024],buf2[1024];
+	while(gets(buf)){
+		gets(buf2);
+		my_printf(buf,buf2);
+	}
+	return 0;
+}
