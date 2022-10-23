ce2da5dc60b367f0cc2bea43174d9d2ca9f4870e
Kamil Galazka <you@example.com>
2022-10-23 15:22:35 +0200

    change code between lab2 and lab3

--- lab2/main.c
+++ lab2/main.c
@@ -5,51 +5,30 @@
 
 int my_printf(char *format_string, char *param) {
     for (int i = 0; i < strlen(format_string); i++) {
-        if (format_string[i] == '#') {
-            if ((format_string[i + 1] == '.') && isdigit(format_string[i + 2]) && (format_string[i + 3] == 'k')) {
-                int len = atoi(&format_string[i + 2]);
-                if (strlen(param) < len)
-                    len = strlen(param);
-                for (int j = 0; j < len; j++) {
-                    if (islower(param[j])) {
-                        if (isdigit(param[j]))
-                            printf("%c", param[j]);
-                        else
-                            printf("%c", toupper(param[j]));
-                    } else {
-                        if (isdigit(param[j]))
-                            printf("%c", param[j]);
-                        else
-                            printf("%c", tolower(param[j]));
-                    }
+        if ((format_string[i] == '#') && (format_string[i + 1] == '.') && isdigit(format_string[i + 2]) &&
+            (format_string[i + 3] == 'k')) {
+            int len = atoi(&format_string[i + 2]);
+            if (strlen(param) < len)
+                len = strlen(param);
+            for (int j = 0; j < len; j++) {
+                if (islower(param[j])) {
+                    if (isdigit(param[j]))
+                        printf("%c", param[j]);
+                    else
+                        printf("%c", toupper(param[j]));
+                } else {
+                    if (isdigit(param[j]))
+                        printf("%c", param[j]);
+                    else
+                        printf("%c", tolower(param[j]));
                 }
-
-                i += 3;
-            } else if (isdigit(format_string[i + 1]) && (format_string[i + 2] == 'k')) {
-                int len = atoi(&format_string[i + 1]);
-                if (strlen(param) < len)
-                    len = strlen(param);
-                for (int j = 0; j < len; j++) {
-                    if (islower(param[j])) {
-                        if (isdigit(param[j]))
-                            printf("%c", param[j]);
-                        else
-                            printf("%c", toupper(param[j]));
-                    } else {
-                        if (isdigit(param[j]))
-                            printf("%c", param[j]);
-                        else
-                            printf("%c", tolower(param[j]));
-                    }
-                }
-
-                i += 2;
             }
+
+            i += 3;
         } else
             putchar(format_string[i]);
     }
     puts("");
-    return 0;
 }
 
 int main(int argc, char *argv[]) {
--- lab3/main.c
+++ lab3/main.c
@@ -3,48 +3,61 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-int my_printf(char *format_string, char *param)
-{
-	for (int i = 0; i < strlen(format_string); i++)
-	{
-		if ((format_string[i] == '#') && (format_string[i + 1] == '.') && isdigit(format_string[i + 2]) && (format_string[i + 3] == 'k'))
-		{
-			int len = atoi(&format_string[i + 2]);
-			if (strlen(param) < len)
-				len = strlen(param);
-			for (int j = 0; j < len; j++)
-			{
-				if (islower(param[j]))
-				{
-					if (isdigit(param[j]))
-						printf("%c", param[j]);
-					else
-						printf("%c", toupper(param[j]));
-				}
-				else
-				{
-					if (isdigit(param[j]))
-						printf("%c", param[j]);
-					else
-						printf("%c", tolower(param[j]));
-				}
-			}
+int my_printf(char *format_string, char *param) {
+    for (int i = 0; i < strlen(format_string); i++) {
+        if ((format_string[i] == '#') && (format_string[i + 1] == '.') && isdigit(format_string[i + 2]) &&
+            (format_string[i + 3] == 'k')) {
+            int len = atoi(&format_string[i + 2]);
+            if (strlen(param) < len)
+                len = strlen(param);
+            for (int j = 0; j < len; j++) {
+                if (islower(param[j])) {
+                    if (isdigit(param[j]))
+                        printf("%c", param[j]);
+                    else
+                        printf("%c", toupper(param[j]));
+                } else {
+                    if (isdigit(param[j]))
+                        printf("%c", param[j]);
+                    else
+                        printf("%c", tolower(param[j]));
+                }
+            }
 
-			i += 3;
-		}
-		else
-			putchar(format_string[i]);
-	}
-	puts("");
+            i += 3;
+        } else if ((format_string[i] == '#') && isdigit(format_string[i + 1]) && (format_string[i + 2] == 'k')) {
+            int len = atoi(&format_string[i + 1]);
+            if (strlen(param) < len)
+                len = strlen(param);
+            for (int j = 0; j < len; j++) {
+                if (islower(param[j])) {
+                    if (isdigit(param[j]))
+                        printf("%c", param[j]);
+                    else
+                        printf("%c", toupper(param[j]));
+                } else {
+                    if (isdigit(param[j]))
+                        printf("%c", param[j]);
+                    else
+                        printf("%c", tolower(param[j]));
+                }
+            }
+
+            i += 2;
+        } else {
+            putchar(format_string[i]);
+        }
+    }
+
+    puts("");
+    return 0;
 }
 
-int main(int argc, char *argv[])
-{
-	char buf[1024], buf2[1024];
-	while (gets(buf))
-	{
-		gets(buf2);
-		my_printf(buf, buf2);
-	}
-	return 0;
+int main(int argc, char *argv[]) {
+    char buf[1024], buf2[1024];
+    while (gets(buf)) {
+        gets(buf2);
+        my_printf(buf, buf2);
+    }
+    return 0;
 }
