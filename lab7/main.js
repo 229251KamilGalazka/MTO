b6d19e7e6af4849b67a78af5df94aa22b9eaf407
Kamil Galazka <you@example.com>
2022-12-18 14:26:13 +0100

    first changes

--- lab7/main.js
+++ lab7/main.js
@@ -6,7 +6,21 @@ var lingeringLine = "";
 
 function my_printf(format_string,param){
 	for(var i=0;i<format_string.length;i++){
-		if((format_string.charAt(i) == '#') && (format_string.charAt(i+1) == 'k')){
+		if((format_string.charAt(i) == '#') && (format_string.charAt(i+1) == 'j')){
+
+			param = param.split('')
+
+			for(const char in param){
+				if(isNaN(char)) {
+					switch(char){
+						case A:
+							
+					}
+				}
+			}
+
+
+
 			process.stdout.write(param);
 			i++;
 		}else{
